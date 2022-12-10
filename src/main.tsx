9e24e77ce16c493f3b01d41c19974a7044d62dfa
ImLecus <marcosgf2005@gmail.com>
2022-12-10 10:08:41 +0100

    Adding production folder

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import Header from './components/header'
 import Main from './components/main'
 import Footer from './components/footer'
-import '../dist/style.css'
-import '../dist/output.css'
+import '../css/style.css'
+import '../css/output.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
