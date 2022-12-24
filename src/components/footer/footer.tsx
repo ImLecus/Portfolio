0badf8eecff8441318c23d5dea966a066d4c6077
ImLecus <marcosgf2005@gmail.com>
2022-12-24 16:50:19 +0100

    Added social media links on the footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,8 +1,14 @@
 import "./footer.css"
 function Footer(){
     return(
-        <footer className="border-zinc-700">
-            <p className="text-zinc-300">&copy; Marcos González 2022-2023 All rights reserved. <br /><br />Frontend Developer</p>
+        <footer  className="border-zinc-700">
+            <div>
+                <a href="https://instagram.com/imlecus"><img src="./img/instagram.svg" alt="" /></a>
+                <a href="https://twitter.com/ImLecus"><img src="./img/twitter.svg" alt="" /></a>
+                <a href="https://github.com/ImLecus"><img src="./img/github-white.svg" alt="" /></a>
+                <a href="https://www.linkedin.com/in/marcosgf"><img src="./img/linkedin.svg" alt="" /></a>
+            </div>
+            <p>&copy; Marcos González 2022-2023 All rights reserved. <br /><br />Frontend Developer</p>
         </footer>
     );
 }
