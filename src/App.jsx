2a6fe1758c9e8b03801c096a948986c6d0d99b8e
ImLecus <marcosgf2005@gmail.com>
2023-04-19 19:26:42 +0200

    Unified "footer" and "main" components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { createContext, useState } from "react";
 
 import Header from "./components/header";
 import Main from "./components/main";
-import Footer from "./components/footer";
 
 import "./style.css";
 
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -87,89 +87,98 @@ export default function Main() {
 
   const lang = useContext(userContext);
   return (
-    <main>
-      <div className="space"></div>
-      <section id={language.header[lang][0]}>
-        <h3>{language.presentation[lang][0]}</h3>
-        <h1 className="my-5">
-          <div>
-            <span>M</span>
-            <span>a</span>
-            <span>r</span>
-            <span>c</span>
-            <span>o</span>
-            <span>s</span>
+    <>
+      <main>
+        <div className="space"></div>
+        <section id={language.header[lang][0]}>
+          <h3>{language.presentation[lang][0]}</h3>
+          <h1 className="my-5">
+            <div>
+              <span>M</span>
+              <span>a</span>
+              <span>r</span>
+              <span>c</span>
+              <span>o</span>
+              <span>s</span>
+            </div>
+            <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            <div>
+              <span>G</span>
+              <span>o</span>
+              <span>n</span>
+              <span>z</span>
+              <span>á</span>
+              <span>l</span>
+              <span>e</span>
+              <span>z</span>
+            </div>
+          </h1>
+          <h3>Frontend + React Native Developer</h3>
+          <img id="pfp" />
+        </section>
+        <div className="space"></div>
+        <section id={language.header[lang][0]}>
+          <div className="flex justify-center">
+            <div className="justify-center items-center">
+              <h2>{language.header[lang][0]}</h2>
+              <p className="items-center" data-aos="fade-right">
+                <br />
+                <br />
+                {language.about[lang][0]}
+                <br />
+                <br />
+                {language.about[lang][1]}
+                <br />
+                <br />
+                {language.about[lang][2]}
+              </p>
+            </div>
           </div>
-          <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-          <div>
-            <span>G</span>
-            <span>o</span>
-            <span>n</span>
-            <span>z</span>
-            <span>á</span>
-            <span>l</span>
-            <span>e</span>
-            <span>z</span>
+          <h3 className="mt-9 text-zinc-400">{language.tech[lang][0]}</h3>
+          <div id="root" className="slides flex flex-wrap justify-center">
+            <Tech
+              name={language.version_control[lang][0]}
+              src="./img/github.svg"
+              description={language.version_control[lang][1]}
+            />
+            <Tech
+              name={language.framework[lang][0]}
+              src="./img/react.svg"
+              description={language.framework[lang][1]}
+            />
+            <Tech
+              name={language.preprocessor[lang][0]}
+              src="./img/sass.svg"
+              description={language.preprocessor[lang][1]}
+            />
+            <Tech
+              name={language.mobile[lang][0]}
+              src="./img/android.svg"
+              description={language.mobile[lang][1]}
+            />
           </div>
-        </h1>
-        <h3>Frontend + React Native Developer</h3>
-        <img id="pfp" />
-      </section>
-      <div className="space"></div>
-      <section id={language.header[lang][0]}>
-        <div className="flex justify-center">
-          <div className="justify-center items-center">
-            <h2>{language.header[lang][0]}</h2>
-            <p className="items-center" data-aos="fade-right">
-              <br />
-              <br />
-              {language.about[lang][0]}
-              <br />
-              <br />
-              {language.about[lang][1]}
-              <br />
-              <br />
-              {language.about[lang][2]}
-            </p>
+        </section>
+        <section id={language.header[lang][1]}>
+          <h2>{language.header[lang][1]}</h2>
+          <div className="flex justify-around flex-wrap">
+            <Card src="hyassets" site="hyassets.netlify.app" />
+            <Card src="astra" site="astracss.netlify.app" />
+            <Card src="platinum" site="platinum-theme.netlify.app" />
           </div>
-        </div>
-        <h3 className="mt-9 text-zinc-400">{language.tech[lang][0]}</h3>
-        <div id="root" className="slides flex flex-wrap justify-center">
-          <Tech
-            name={language.version_control[lang][0]}
-            src="./img/github.svg"
-            description={language.version_control[lang][1]}
-          />
-          <Tech
-            name={language.framework[lang][0]}
-            src="./img/react.svg"
-            description={language.framework[lang][1]}
-          />
-          <Tech
-            name={language.preprocessor[lang][0]}
-            src="./img/sass.svg"
-            description={language.preprocessor[lang][1]}
-          />
-          <Tech
-            name={language.mobile[lang][0]}
-            src="./img/android.svg"
-            description={language.mobile[lang][1]}
-          />
-        </div>
-      </section>
-      <section id={language.header[lang][1]}>
-        <h2>{language.header[lang][1]}</h2>
-        <div className="flex justify-around flex-wrap">
-          <Card src="hyassets" site="hyassets.netlify.app" />
-          <Card src="astra" site="astracss.netlify.app" />
-          <Card src="platinum" site="platinum-theme.netlify.app" />
-        </div>
-      </section>
-      <section id={language.header[lang][2]}>
-        <h2>{language.header[lang][2]}</h2>
-        <div className="h-16"></div>
-        <Contact />
-      </section>
-    </main>
+        </section>
+        <section id={language.header[lang][2]}>
+          <h2>{language.header[lang][2]}</h2>
+          <div className="h-16"></div>
+          <Contact />
+        </section>
+      </main>
+      <footer>
+        <p>
+          &copy; Marcos González 2023-2024 All rights reserved. <br />
+          <br />
+          Frontend Developer
+        </p>
+      </footer>
+    </>
   );
 }
